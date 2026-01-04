4da2a8fd8457b2cc17715e2f663b0377255101ca
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-03 19:42:32 -0500

    Add transfer module stub for future implementation

---
+++ hyprkvm-daemon/src/transfer/mod.rs
@@ -0,0 +1,7 @@
+//! Control transfer module
+//!
+//! Manages the transfer of keyboard/mouse control between machines.
+
+// TODO: Sprint 4 - Implement control transfer
+// pub mod manager;
+// pub mod state_machine;
